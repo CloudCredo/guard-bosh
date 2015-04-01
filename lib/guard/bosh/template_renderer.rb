f3e09b20d5cabc74b841d9befd3480a8bcf86833
Andrew Crump <andrew@cloudcredo.com>
2015-04-01 23:16:57 +0100

    Improve template error handling
    [#91550966]

--- lib/guard/bosh/template_renderer.rb
+++ lib/guard/bosh/template_renderer.rb
@@ -8,16 +8,32 @@ module Guard
       def render(context:, template:)
         renderer = ::Bosh::Template::Renderer.new(
           context: JSON.generate(context))
+
+        # The re-writing of messages we do here is intended to make the output
+        # more readable, but may not be wise.
         begin
           renderer.render(template)
           { template: template, status: :success, detail: '' }
         rescue ::Bosh::Template::UnknownProperty => e
-          {
-            template: template,
-            status: :failure,
-            detail: "Missing property: #{e.name}"
-          }
+          error(template, "missing property: #{e.name}")
+        rescue NoMethodError => e
+          error(template, e.message.sub(/ for #<[^>]+>$/, ''))
+        rescue NameError => e
+          error(template, e.message.sub(/ for #<[^>]+>$/, ''))
+        rescue SyntaxError => e
+          error(template, e.message.split("\n").first.sub(
+            /^\(erb\):[0-9]+: /, ''))
         end
+      end
+
+      private
+
+      def error(template, message)
+        {
+          template: template,
+          status: :failure,
+          detail: message
+        }
       end
     end
   end
--- spec/guard/bosh/template_renderer_spec.rb
+++ spec/guard/bosh/template_renderer_spec.rb
@@ -7,10 +7,14 @@ describe Guard::Bosh::TemplateRenderer do
     Guard::Bosh::TemplateRenderer.new
   end
 
+  let(:bosh_renderer) { instance_double(::Bosh::Template::Renderer) }
+  before do
+    expect(::Bosh::Template::Renderer).to receive(:new).with(
+      context: '{}').and_return(bosh_renderer)
+  end
+
   context 'when the template contains no errors' do
     it 'reports that no error occurred' do
-      bosh_renderer = instance_double(::Bosh::Template::Renderer)
-      expect(::Bosh::Template::Renderer).to receive(:new).with(context: '{}').and_return(bosh_renderer)
       expect(bosh_renderer).to receive(:render).with('config.erb')
       result = subject.render(context: {}, template: 'config.erb')
       expect(result).to eq(template: 'config.erb', status: :success, detail: '')
@@ -19,11 +23,53 @@ describe Guard::Bosh::TemplateRenderer do
 
   context 'when the template refers to an unknown property' do
     it 'reports the missing property' do
-      bosh_renderer = instance_double(::Bosh::Template::Renderer)
-      expect(::Bosh::Template::Renderer).to receive(:new).with(context: '{}').and_return(bosh_renderer)
-      expect(bosh_renderer).to receive(:render).with('config.erb').and_raise(::Bosh::Template::UnknownProperty.new('redis.port'))
+      expect(bosh_renderer).to receive(:render).with('config.erb').and_raise(
+        ::Bosh::Template::UnknownProperty.new('redis.port'))
       result = subject.render(context: {}, template: 'config.erb')
-      expect(result).to eq(template: 'config.erb', status: :failure, detail: 'Missing property: redis.port')
+      expect(result).to eq(
+        template: 'config.erb',
+        status: :failure,
+        detail: 'missing property: redis.port'
+      )
+    end
+  end
+
+  context 'when the template calls a misnamed helper method' do
+    it 'reports the missing helper method' do
+      expect(bosh_renderer).to receive(:render).with('config.erb').and_raise(
+        NoMethodError.new("undefined method `o' for #<Bosh::Template::EvaluationContext:0x00000000000000>"))
+      result = subject.render(context: {}, template: 'config.erb')
+      expect(result).to eq(
+        template: 'config.erb',
+        status: :failure,
+        detail: "undefined method `o'"
+      )
+    end
+  end
+
+  context 'when the template references a missing name' do
+    it 'reports the missing name' do
+      expect(bosh_renderer).to receive(:render).with('config.erb').and_raise(
+        NameError.new("undefined local variable or method `missing' for #<Bosh::Template::EvaluationContext:0x00000000000000>"))
+      result = subject.render(context: {}, template: 'config.erb')
+      expect(result).to eq(
+        template: 'config.erb',
+        status: :failure,
+        detail: "undefined local variable or method `missing'"
+      )
+    end
+  end
+
+  context 'when the template is not well-formed' do
+    it 'reports the template error' do
+      expect(bosh_renderer).to receive(:render).with('config.erb').and_raise(
+        SyntaxError.new("(erb):7: syntax error, unexpected end-of-input, expecting keyword_end\n; _erbout.force_encoding(__ENCODING__)"))
+      result = subject.render(context: {}, template: 'config.erb')
+      expect(result).to eq(
+        template: 'config.erb',
+        status: :failure,
+        detail: 'syntax error, unexpected end-of-input, expecting keyword_end'
+      )
     end
   end
 end
